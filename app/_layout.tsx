57b8c6612f130b4a79eb77db93d515164bfea656
Diego Cardenas <diego.cardenasaleg@gmail.com>
2024-07-28 19:18:01 -0500

    added icon and splashscreen config to app

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,21 +2,31 @@ import "react-native-gesture-handler";
 import "react-native-reanimated";
 
 import { useFonts } from "expo-font";
+import * as SplashScreen from "expo-splash-screen";
 
 import { NavigationContainer } from "@react-navigation/native";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import Toast from 'react-native-toast-message';
+import Toast from "react-native-toast-message";
 
 import { Loader } from "@/components";
 import { UserProvider } from "@/state";
 import { RootStackNavigator } from "@/navigation/RootNavigation";
+import { useEffect } from "react";
 
 const queryClient = new QueryClient();
+
+SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
   const [loaded] = useFonts({
     SpaceMono: require("../assets/fonts/SpaceMono-Regular.ttf")
   });
+
+  useEffect(() => {
+    if (loaded) {
+      SplashScreen.hideAsync();
+    }
+  }, [loaded]);
 
   if (!loaded) {
     return <Loader />;
